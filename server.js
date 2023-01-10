f944253ef9d22cf536fce3238a1a2d2904849557
Adriele Garcia <adrielegarcia101@gmail.com>
2023-01-10 12:49:43 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
-// Server.js
-import jsonServer from "json-server"
+// import { json } from 'body-parser';
+// import jsonServer from 'json-server';
+// const server = jsonServer.create();
+// const router = jsonServer.router('db.json');
+// const middlewares =  jsonServer.defaults();
+// const port = process.env.PORT. || 3200;
+
+// server.use(middlewares);
+// server.use(router);
+
+// server.listen(port);
+
+const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
-server.use(middlewares)
+server.use (middlewares)
 server.use(router)
 server.listen(3000, () => {
-console.log('JSON Server is running')
+    console.log('JSON Server is running')
 })
